a900777a917a71ae39de3f232c5692e835b483f9
João Vitor da Silva <silva.joao1@outlook.com.br>
2022-08-04 17:38:23 -0300

    feat: styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,1 +1,10 @@
-export const App = () => <p>Todo list</p>;
+import { ThemeProvider } from 'styled-components';
+import { GlobalStyle } from './styles/global';
+
+import { defaultTheme } from './styles/themes/default';
+
+export const App = () => (
+  <ThemeProvider theme={defaultTheme}>
+    <GlobalStyle />
+  </ThemeProvider>
+);
