dbf4d6d9c6a4b9720110d5003eaa0b880223e734
João Vitor da Silva <silva.joao1@outlook.com.br>
2022-08-04 18:14:21 -0300

    feat: home header layout

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,1 +1,17 @@
-export const Home = () => <p>Home</p>;
+import logo from '../../assets/logo.svg';
+import plus from '../../assets/plus.svg';
+import { CreateButton, Header, Input, InputContainer } from './styles';
+
+export const Home = () => (
+  <Header>
+    <img src={logo} alt="Foguete a esquerda e escrito todo a direita" />
+
+    <InputContainer>
+      <Input placeholder="Adicione uma nova tarefa" />
+      <CreateButton>
+        <p>Criar</p>
+        <img src={plus} alt="Ícone com um circulo e um sinal de mais dentro" />
+      </CreateButton>
+    </InputContainer>
+  </Header>
+);
