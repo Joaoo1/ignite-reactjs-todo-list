630ed572afbb841de41c16a2527b868f0be4b00e
João Vitor da Silva <silva.joao1@outlook.com.br>
2022-08-04 18:43:01 -0300

    feat: No tasks found component

---
+++ src/pages/Home/components/NoTasksFound/styles.ts
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+export const NoTasksFoundContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding-top: 4rem;
+
+  > img {
+    margin-bottom: 16px;
+  }
+`;
